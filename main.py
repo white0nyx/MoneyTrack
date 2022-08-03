2cdb6d25f5d394673f20c23e6fd9676f10b3d67d
Anatoly Kushnarenko <Anatoly.kushnarenko@gmail.com>
2022-08-03 04:52:12 +0300

    Fixed menu_crt_new_acc. Added the ability to specify the account name.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
 from main_window import Ui_MainWindow
-from menu_crt_new_acc import MenuCreateAccount
+from menu_crt_new_acc import Ui_menu_create_account
 
 app = QtWidgets.QApplication(sys.argv)
 app.setStyle('Fusion')
@@ -15,7 +15,7 @@ MainWindow.show()
 def openWindowCrtNewAcc():
     global menu_create_account
     menu_create_account = QtWidgets.QDialog()
-    window_crt_new_acc = MenuCreateAccount()
+    window_crt_new_acc = Ui_menu_create_account()
     window_crt_new_acc.setupUi(menu_create_account)
     menu_create_account.show()
 
